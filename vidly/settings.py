d6dfdbcdec75af7a164a22b4b29fcae37900846a
Karan Mahato <44karanmahato@gmail.com>
2023-06-17 12:35:46 +0545

    add api w/ tastypie

--- vidly/settings.py
+++ vidly/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 import os
 INSTALLED_APPS = [
     'movies.apps.MoviesConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
