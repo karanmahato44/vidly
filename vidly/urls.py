65103ef9fd645ac7418dbf0982b642bdf439f0e1
Karan Mahato <44karanmahato@gmail.com>
2023-06-17 16:31:44 +0545

    prepared for deployment to heroku / gunicorn, whitenoise

--- vidly/urls.py
+++ vidly/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from api.models import MovieResource
+from . import views
 
 movie_resource = MovieResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='homepage'),
     path('movies/', include('movies.urls')),
     path('api/', include(movie_resource.urls))
 ]
